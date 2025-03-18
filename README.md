386b593429b394678f36b5b727d3cfa7245a648f
OMAR <omar@localhost>
2025-03-18 05:07:24 +0100

    refactor: remove outdated README and add new project documentation feat: implement GUI for capturing sign language positions using customtkinter chore: update dependencies in pyproject.toml and poetry.lock

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ This project consists of three main modules:
 2. Model Trainer: Trains machine learning models for sign language recognition
 3. Recognition Module: Deploys trained models for real-time sign language interpretation
 
+## Inspiration
+
+ - Hand Tracking using MediaPipe from Sousannah (see [https://github.com/Sousannah/hand-tracking-using-mediapipe](https://github.com/Sousannah/hand-tracking-using-mediapipe))
+ - [Mudra signs](https://naruto.fandom.com/fr/wiki/Mudr%C3%A2) in Naruto
+
 ## License
 
 This project is licensed under the GNU GPLv3 License - see the LICENSE file for details.
--- dataset_generator_helper/main.py
+++ dataset_generator_helper/main.py
@@ -8,3 +8,58 @@
 # 6. Fermer la fenetre de capture
 # 7. Sauvegarder le dataset dans un fichier
 
+import customtkinter
+
+positions = []
+
+WIDTH = 500
+HEIGHT = 400
+
+class AddPosition(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("400x300")
+        self.title("Add position")
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.label = customtkinter.CTkLabel(self, text="Give a label to the position (if blank the position will have index as label)")
+        self.label.grid(row=0, column=0, sticky="ew")
+
+class App(customtkinter.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.geometry(str(HEIGHT) + "x" + str(WIDTH))
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.label = customtkinter.CTkLabel(self, text="List of position")
+        self.label.grid(row=0, column=0, sticky="ew")
+
+        self.textbox = customtkinter.CTkTextbox(master=self, width=400, corner_radius=0)
+        self.textbox.grid(row=1, column=0, sticky="nsew")
+        self.textbox.insert("0.0", "Some example text!\n" * 50)
+
+        self.button = customtkinter.CTkButton(self, text="Capture position", command=self.capture_position)
+        self.button.grid(row=2, column=0, sticky="ew")
+    
+        self.capture_window = None
+    
+    def print_positions(self):
+        i = 0
+        for position in positions:
+            i += 1
+            self.textbox.insert("end", str(i) + ". " + position + "\n")
+
+    def capture_position(self):
+        if self.capture_window is None or not self.capture_window.winfo_exists():
+            self.capture_window = AddPosition(self)  # create window if its None or destroyed
+        self.capture_window.focus()  # if window exists focus it
+
+
+app = App()
+app.mainloop()
