76d19db0ccfc5183eb295c93bff557175665629c
OMAR <omar@localhost>
2025-04-13 00:05:01 +0200

    feat: add preview position dimensions to configs and update ScrollableLabels to display images

--- dataset_generator_helper/scrollable_label.py
+++ dataset_generator_helper/scrollable_label.py
@@ -1,5 +1,8 @@
 import customtkinter as ctk
 from position_manager import PositionManager
+from configs import PREVIEW_POS_H, PREVIEW_POS_W
+from PIL import Image
+
 
 class ScrollableLabels(ctk.CTkScrollableFrame):
     def __init__(self, master, position_manager : PositionManager, **kwargs):
@@ -22,14 +25,18 @@ class ScrollableLabels(ctk.CTkScrollableFrame):
         self.clean()
 
         row = 0
-        for k in positions.keys():
-            label = remove_btn = None
+        for k,v in positions.items():
+            label = remove_btn = image = frame_img = None
+
+            image = ctk.CTkImage(dark_image=v['img'], size=(PREVIEW_POS_W,PREVIEW_POS_H))
+            frame_img = ctk.CTkLabel(self, image=image, text="")
 
             label = ctk.CTkLabel(self, text=k)
             remove_btn = ctk.CTkButton(self, text="Remove", command= lambda x=k: self.pm.remove_position(x))
 
-            label.grid(row=row, column=0, sticky="nsew")
-            remove_btn.grid(row=row, column=1, sticky="nsew")
+            frame_img.grid(row=row, column=0, sticky="nsew", pady=4)
+            label.grid(row=row, column=1, sticky="nsew", pady=4)
+            remove_btn.grid(row=row, column=2, sticky="nsew", pady=4)
 
-            self.list.append((label, remove_btn))
+            self.list.append((frame_img, label, remove_btn))
             row += 1
